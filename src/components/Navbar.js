ec5aa9b082b604d77ac764741fbbe29bf4053dab
Manish Baghel <manish_11914130@nitkkr.ac.in>
2022-09-08 18:37:54 +0530

    final touches

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,14 +24,12 @@ export default function Navbar(props) {
                         <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height: "30px", width: "30px",cursor:'pointer'}}></div>
                         <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height: "30px", width: "30px",cursor:'pointer'}}></div>
                         <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height: "30px", width: "30px",cursor:'pointer'}}></div>
+                        <div className="bg-light rounded mx-2" onClick={()=>{props.toggleMode('light')}} style={{height: "30px", width: "30px",cursor:'pointer'}}></div>
+                        <div className="bg-dark rounded mx-2" onClick={()=>{props.toggleMode('dark')}} style={{height: "30px", width: "30px",cursor:'pointer'}}></div>
                     </div>
-                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                    {/* <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
                         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
                         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
-                    </div>
-                    {/* <div className="custom-control custom-switch">
-                        <input type="checkbox" className="custom-control-input" id="customSwitch1"/>
-                        <label className="custom-control-label" for="customSwitch1">Toggle this switch element</label>
                     </div> */}
                 </div>
             </div>
